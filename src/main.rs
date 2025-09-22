fb84859e01ca7a0482a14f20b158425010d0f7c2
Antonio Garcés <garces080995@gmail.com>
2025-09-22 13:29:35 +0200

    Change logs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 use axum::Router;
 use axum::extract::Query;
 use axum::routing::get;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::EnvFilter;
 
 async fn hello(Query(params): Query<HashMap<String, String>>) -> String {
     let name = params
@@ -14,23 +16,34 @@ async fn hello(Query(params): Query<HashMap<String, String>>) -> String {
 #[tokio::main]
 async fn main() {
     // initialize tracing
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .with_max_level(tracing::Level::DEBUG)
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .compact()
+        .init();
 
     // initialize env vars
     dotenvy::dotenv().ok();
 
     //build app
     let app = Router::new().route("/", get(hello)).layer((
-        tower_http::trace::TraceLayer::new_for_http(),
+        tower_http::trace::TraceLayer::new_for_http()
+            .make_span_with(tower_http::trace::DefaultMakeSpan::new())
+            .on_response(tower_http::trace::DefaultOnResponse::new()),
         tower_http::timeout::TimeoutLayer::new(std::time::Duration::from_secs(5)),
     ));
 
     let host = std::env::var("HOST").unwrap_or("127.0.0.1".to_string());
     let port = std::env::var("PORT").unwrap_or("3000".to_string());
-    let listener = tokio::net::TcpListener::bind(format!("{}:{}", host, port))
+    let listener = tokio::net::TcpListener::bind(format!("{host}:{port}"))
         .await
         .unwrap();
-    tracing::debug!("Listening on {}", listener.local_addr().unwrap());
+    tracing::info!("Listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
         .await
